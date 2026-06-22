c61e86d90d69301f4cb0a38b885ba5f3b0f98ac2
Taha Hundekari <ge47mut@mytum.de>
2026-06-22 13:45:29 +0200

    feat: use java v25 in spring services

--- services/spring-logbook/app/build.gradle.kts
+++ services/spring-logbook/app/build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // JSON serialization
+    implementation("com.fasterxml.jackson.core:jackson-databind")
     
     // Database Persistence
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
